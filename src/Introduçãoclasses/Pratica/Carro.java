334884f54def6d03357161c05dbde34b7959214d
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-06-15 22:43:32 -0300

    Revisando

---
+++ src/Introduçãoclasses/Pratica/Carro.java
@@ -0,0 +1,37 @@
+package Introduçãoclasses.Pratica;
+
+public class Carro {
+
+    String nome;
+    String cor;
+    int ano;
+    boolean andando;
+
+    void status() {
+        System.out.println("Qual  o nome " + this.nome);
+        System.out.println("Qual o ano" + this.ano);
+        System.out.println("E a cor" + this.cor);
+        System.out.println("Esta andando?" + this.andando);
+
+    }
+
+    void emMovimento() {
+
+        if (this.andando == true){
+
+            System.out.println("Veiculo em movimento");
+
+        } else {
+            System.out.println("Esta parado");
+        }
+
+
+
+    }
+
+
+    void parado() {
+
+    }
+
+}
---
+++ src/Introduçãoclasses/Pratica/CarroTest01.java
@@ -0,0 +1,18 @@
+package Introduçãoclasses.Pratica;
+
+public class CarroTest01 {
+
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+
+        c1.nome = "Gol";
+        c1.cor =  "Preto";
+        c1.ano = 2000;
+        c1.andando = false;
+        c1.status();
+        c1.emMovimento();
+        c1.parado();
+
+
+    }
+}
