daff00bc33c3a6400d44f6e70784fc3cab2ef4b0
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-06-21 01:38:31 -0300

    Preciso pratica

---
+++ src/Introduçãoclasses/pratica04/CarroTest.java
@@ -0,0 +1,15 @@
+package Introduçãoclasses.pratica04;
+
+public class CarroTest {
+
+    public static void main(String[] args) {
+
+        Carro carro01 = new Carro();
+        carro01.setCor("Azul");
+        System.out.println("Cor " + carro01.getCor());
+        Carro carro02 = new Carro("Preto","Gol", 20);
+
+
+
+    }
+}
